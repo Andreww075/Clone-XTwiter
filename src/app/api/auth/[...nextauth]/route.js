a01fa7986ac45a9a1e3070689df5148b39861e2d
Nicolás Gómez <Nvandres079@gmail.com>
2024-05-09 09:16:21 -0500

    Add mini-profile to the sidebar

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-const handler = NextAuth ({
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+const handler = NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -10,13 +10,15 @@ const handler = NextAuth ({
     // ...add more providers here
   ],
   callbacks: {
-    async session({session, token}) {
-      session.user.username = session.user.name;
+    async session({ session, token }) {
+      session.user.username = session.user.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
       session.user.uid = token.sub;
       return session;
-    }
-  }
+    },
+  },
 });
 
-
-export { handler as GET, handler as POST } 
+export { handler as GET, handler as POST };
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,39 +4,61 @@ import { FaXTwitter } from "react-icons/fa6";
 import { AiFillHome } from "react-icons/ai";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { HiDotsHorizontal } from 'react-icons/hi'
+import Image from "next/image";
 
 export default function Sidebar() {
   const { data: session } = useSession();
   console.log(session);
 
   return (
-    <div className="flex flex-col gap-4 p-3">
-      <Link href="/">
-        <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition duration-200 " />
-      </Link>
+    <div className="flex flex-col p-3 justify-between h-screen">
+      <div className="flex flex-col gap-4">
+        <Link href="/">
+          <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition duration-200 " />
+        </Link>
 
-      <Link
-        href="/"
-        className="flex items-center p-3 hover:bg-gray-100 rounded-full transition duration-200 gap-2 w-fit"
-      >
-        <AiFillHome className="w-7 h-7" />
-        <span className="font-bold hidden lg:inline">Home</span>
-      </Link>
+        <Link
+          href="/"
+          className="flex items-center p-3 hover:bg-gray-100 rounded-full transition duration-200 gap-2 w-fit"
+        >
+          <AiFillHome className="w-7 h-7" />
+          <span className="font-bold hidden lg:inline">Home</span>
+        </Link>
 
-      {session ? (
-        <button
-          onClick={signOut}
-          className="bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold"
-        >
-          Sign Out
-        </button>
-      ) : (
-        <button
-          onClick={signIn}
-          className="bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold"
-        >
-          Sign In
-        </button>
+        {session ? (
+          <button
+            onClick={signOut}
+            className="bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold"
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            onClick={signIn}
+            className="bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold"
+          >
+            Sign In
+          </button>
+        )}
+      </div>
+
+      {session && (
+        <div className="text-gray-700 text-sm flex items-center cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200">
+          <Image
+            loader={() => session.user.image}
+            src={session.user.image}
+            width={35}
+            height={35}
+            alt="user-img"
+            className="rounded-full xl:mr-2"
+          />
+          <div className="hidden xl:inline">
+            <h4 className="font-bold">{session.user.name}</h4>
+            <p className="text-gray-500">@{session.user.username}</p>
+          </div>
+          <HiDotsHorizontal className="h-5 xl:ml-8 hidden xl:inline" />
+        </div>
       )}
     </div>
   );
